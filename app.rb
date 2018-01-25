2cb3d5aa27e46ffffaf5b777ae38ffd86f3ad4f8
itggot-Lukas-Andersson <lukas.andersson1@itggot.se>
2018-01-25 10:47:33 +0100

    Lade till "View" samt ändrade CSS och HTML

--- app.rb
+++ app.rb
@@ -8,11 +8,14 @@ get('/') do
 end
 
 get('/view/:id') do
-
+	db = SQLite3::Database.new("./db/plebbit.sqlite")
+	id = params[:id]
+	post = db.execute('SELECT * FROM posts WHERE id IS ' + id)
+	erb(:view, locals:{post:post})
 end
 
 get('/post') do
-
+	
 end
 
 post('/create') do
