379bacba23277a07846f204ba0de21f1a42a9ab1
itggot-Lukas-Andersson <lukas.andersson1@itggot.se>
2018-02-01 10:56:37 +0100

    CSS

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ get('/view/:id') do
 	db = SQLite3::Database.new("./db/plebbit.sqlite")
 	id = params[:id]
 	post = db.execute('SELECT * FROM posts WHERE id IS ' + id)
-	erb(:view, locals:{post:post})
+	erb(:view, locals:{posts:post})
 end
 
 get('/post') do
