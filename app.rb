5738bb989706b6e27ede512966a62713ffc2601b
Alex Henryz <alex.henryz@itggot.se>
2018-02-08 10:52:40 +0100

    Finslipning
    XD

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'sqlite3'
 
 get('/') do
-	db = SQLite3::Database.new("./db/plebbit.sqlite")
-	all = db.execute('SELECT * FROM posts')
+	db = SQLite3::Database.new("./db/plebbit.db")
+	all = db.execute('SELECT * FROM posts ORDER BY id DESC')
 	erb(:index, locals:{posts:all})
 end
 
 get('/view/:id') do
-	db = SQLite3::Database.new("./db/plebbit.sqlite")
+	db = SQLite3::Database.new("./db/plebbit.db")
 	id = params[:id]
 	post = db.execute('SELECT * FROM posts WHERE id IS ' + id)
 	erb(:view, locals:{posts:post})
@@ -21,5 +21,18 @@ end
 post('/create') do
 
 end
+
+get('/hot') do
+	db = SQLite3::Database.new("./db/plebbit.db")
+	all = db.execute('SELECT * FROM posts ORDER BY points DESC')
+	erb(:index, locals:{posts:all})
+end
+
+get('/title') do
+	db = SQLite3::Database.new("./db/plebbit.db")
+	all = db.execute('SELECT * FROM posts ORDER BY title')
+	erb(:index, locals:{posts:all})	
+end
+
 
 
